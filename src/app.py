dad1f62bbb600a35c84974ce4988abc067da431b
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-12 15:39:08 -0300

    Refatorando arquivo principal app.py

--- src/app.py
+++ src/app.py
@@ -1,65 +1,16 @@
 from flask import Flask
-from dynaconf import FlaskDynaconf
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_admin import Admin
-import sqlalchemy as sa
-from flask_admin.contrib.sqla import ModelView
+from src.ext import configuration
+from src.ext import database
+from src.ext import migration
+from src.ext import admin
 
 app = Flask(__name__)
-
-FlaskDynaconf(app)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-admin = Admin(app, name='Gearhead', template_mode='bootstrap3')
+configuration.init_app(app)
+database.init_app(app)
+migration.init_app(app)
+admin.init_app(app)
 
 
 @app.route('/')
 def index():
     return 'Olá'
-
-
-class User(db.Model):
-
-    __tablename__ = 'users'
-
-    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    email = sa.Column(sa.String, unique=True, nullable=False)
-    first_name = sa.Column(sa.String, nullable=False)
-    last_name = sa.Column(sa.String, nullable=False)
-    password = sa.Column(sa.String, nullable=False)
-    is_active = sa.Column(sa.Boolean, default=True)
-
-    def __str__(self):
-        return f'\nemail: {self.email}\nfirst_name: {self.first_name}\nlast_name: {self.last_name}\nis_active: {self.is_active}'
-
-
-class Brand(db.Model):
-
-    __tablename__ = 'brands'
-
-    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    name = sa.Column(sa.String, nullable=False, unique=True)
-    code = sa.Column(sa.String, nullable=False, unique=True)
-
-    def __str__(self):
-        return f'\nname: {self.name}\ncode: {self.code}'
-
-
-class Vehicle(db.Model):
-
-    __tablename__ = 'vehicles'
-
-    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    name = sa.Column(sa.String, nullable=False)
-    year = sa.Column(sa.Date, nullable=False)
-    code = sa.Column(sa.String, nullable=False)
-    brand = sa.Column(sa.Integer, sa.ForeignKey('brands.id'))
-
-    def __str__(self):
-        return f'\nname: {self.name}\nyear: {self.year}\ncode: {self.code}\nbrand: {self.brand}'
-
-
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Brand, db.session))
-admin.add_view(ModelView(Vehicle, db.session))
