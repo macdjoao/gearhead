9dbbf896b4bef45efc3906546061bb64ce763a47
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-12 16:51:50 -0300

    fix: Melhorando imports de models em admin.py

--- src/ext/admin.py
+++ src/ext/admin.py
@@ -1,17 +1,17 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from src.ext.database import db
-from src.blueprints.models.user import User
-from src.blueprints.models.brand import Brand
-from src.blueprints.models.vehicle import Vehicle
-from src.blueprints.models.part import Part
+from src.blueprints.models.user import User as UserModel
+from src.blueprints.models.brand import Brand as BrandModel
+from src.blueprints.models.vehicle import Vehicle as VehicleModel
+from src.blueprints.models.part import Part as PartModel
 
 admin = Admin(name='Gearhead', template_mode='bootstrap3')
 
 
 def init_app(app):
     admin.init_app(app)
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Brand, db.session))
-    admin.add_view(ModelView(Vehicle, db.session))
-    admin.add_view(ModelView(Part, db.session))
+    admin.add_view(ModelView(UserModel, db.session))
+    admin.add_view(ModelView(BrandModel, db.session))
+    admin.add_view(ModelView(VehicleModel, db.session))
+    admin.add_view(ModelView(PartModel, db.session))
