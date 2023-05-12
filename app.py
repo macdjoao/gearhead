77fc401cc36ec715721dffb93cd596f571439be4
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-12 13:42:45 -0300

    Adicionando e integrando flask-admin ao projeto

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from dynaconf import FlaskDynaconf
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_admin import Admin
 
 app = Flask(__name__)
 
 FlaskDynaconf(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
 
 
 @app.route('/')
