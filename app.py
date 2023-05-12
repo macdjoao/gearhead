12ef28cb6e8d79b304e58695c68dd1a9294a48c9
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-12 13:24:35 -0300

    Integrando Alchemy e Migrate ao Flask

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from dynaconf import FlaskDynaconf
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 FlaskDynaconf(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
