3e8ce5339dc68286f3416e1ebe956f99f1d9ae67
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-15 15:05:46 -0300

    fix: Atribuindo instancia de Marshmallow() a uma variavel

--- src/ext/serializer.py
+++ src/ext/serializer.py
@@ -1,5 +1,8 @@
 from flask_marshmallow import Marshmallow
 
+# Diferente dos outros factories, 'ma' foi atribuído a uma variável, pois essa variável será necessária em outras partes do código.
+ma = Marshmallow()
+
 
 def init_app(app):
-    Marshmallow(app)
+    ma.init_app(app)
