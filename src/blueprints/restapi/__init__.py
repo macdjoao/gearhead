1310ebe124bd8d9771d4a2fc4ee6fcd19f42fbfa
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-15 10:39:48 -0300

    feat: Desenhando primeiras rotas de User

--- src/blueprints/restapi/__init__.py
+++ src/blueprints/restapi/__init__.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
-from .user import UserResource
+from .user import UserResource, UserListResource
 
-bp = Blueprint("restapi", __name__, url_prefix="/api/v1")
+bp = Blueprint("restapi", __name__, url_prefix="/api/")
 api = Api(bp)
 api.add_resource(UserResource, "/user/")
+api.add_resource(UserListResource, "/user/all")
 
 
 def init_app(app):
--- src/blueprints/restapi/user.py
+++ src/blueprints/restapi/user.py
@@ -2,5 +2,21 @@ from flask_restful import Resource
 
 
 class UserResource(Resource):
+
+    def post(self):
+        return 'post user'
+
     def get(self):
-        return 'olá get user'
+        return 'get user'
+
+    def patch(self):
+        return 'patch user'
+
+    def delete(self):
+        return 'delete user'
+
+
+class UserListResource(Resource):
+
+    def get(self):
+        return 'get list user'
