3f466c479f0a67c94478768ebe37f658dba84472
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-15 15:47:21 -0300

    feat: Adicionando schema de User

--- src/blueprints/schemas/user_schema.py
+++ src/blueprints/schemas/user_schema.py
@@ -2,6 +2,6 @@ from src.ext.serializer import ma
 from src.blueprints.models.user_model import UserModel
 
 
-class UserSchema(ma.SQLAlchemySchema):
+class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = UserModel
