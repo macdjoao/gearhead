b64ad50dd7c1c6277f95ef8a423d5f2deb1db2ec
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-16 10:57:50 -0300

    fix: Corrigindo retorno das rotas

--- src/blueprints/crud/user_crud.py
+++ src/blueprints/crud/user_crud.py
@@ -1,6 +1,7 @@
 from src.blueprints.models.user_model import UserModel
 from src.ext.database import db
 from werkzeug.security import generate_password_hash
+from src.blueprints.schemas.user_schema import UserSchema
 
 
 class UserCRUD:
@@ -12,25 +13,26 @@ class UserCRUD:
             user.password = generate_password_hash(user.password)
             db.session.add(user)
             db.session.commit()
-            return f'User successfully created: {user}'
+            schema = UserSchema()
+            return f'User successfully created: {schema.dump(user)}'
         except Exception as exc:
             print(f'UserCRUD[CREATE_USER] Error: {exc}')
             return f'Create Error'
 
-    # Corrigir retorno
     def read_user(self, id):
         try:
-            query = UserModel.query.filter(UserModel.id == id)
-            return f'User: {query}'
+            user = UserModel.query.filter_by(id=id).first()
+            schema = UserSchema()
+            return f'User: {schema.dump(user)}'
         except Exception as exc:
             print(f'UserCRUD[READ_USER] Error: {exc}')
             return f'Read Error'
 
-    # Corrigir retorno
     def read_users(self):
         try:
-            query = UserModel.query.all()
-            return f'Users: {query}'
+            users = UserModel.query.all()
+            schema = UserSchema(many=True)
+            return f'User: {schema.dump(users)}'
         except Exception as exc:
             print(f'UserCRUD[READ_USERS] Error: {exc}')
             return f'Read Error'
@@ -45,9 +47,9 @@ class UserCRUD:
 
     def delete_user(self, id):
         try:
-            UserModel.query.filter(UserModel.id == id).delete()
+            UserModel.query.filter_by(id=id).delete()
             db.session.commit()
-            return f'User successfully deleted'
+            return f'User successfully deleted.'
         except Exception as exc:
             print(f'UserCRUD[DELETE_USER] Error: {exc}')
             return f'Delete Error'
