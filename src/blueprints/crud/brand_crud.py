f5af3a20ede9cb483e2e1ccf00bf61f72003a86b
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-18 12:31:18 -0300

    feat: Função Read Brand (unitário, por id)

--- src/blueprints/crud/brand_crud.py
+++ src/blueprints/crud/brand_crud.py
@@ -1,6 +1,5 @@
 from src.blueprints.models.brand_model import BrandModel
 from src.ext.database import db
-from werkzeug.security import generate_password_hash
 from src.blueprints.schemas.brand_schema import BrandSchema
 
 
@@ -18,3 +17,14 @@ class BrandCRUD:
         except Exception as exc:
             print(f'BrandCRUD[CREATE_BRAND] Error: {exc}')
             return f'Create Error.'
+
+    def read_brand(self, id: int):
+        try:
+            brand = BrandModel.query.get(id)
+            if brand is None:
+                return 'Brand not found', 404
+            schema = BrandSchema()
+            return f'Brand: {schema.dump(brand)}'
+        except Exception as exc:
+            print(f'BrandCRUD[READ_BRAND] Error: {exc}')
+            return f'Read Error.'
