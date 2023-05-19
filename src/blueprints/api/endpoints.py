78b7125b1d0616a7824febc4276f82a1dc5f18d4
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-19 11:05:40 -0300

    feat: Registrando blueprint da rota brand

--- src/blueprints/api/endpoints.py
+++ src/blueprints/api/endpoints.py
@@ -1,7 +1,9 @@
 from src.blueprints.api.user_routes import user
 from src.blueprints.api.login_route import login
+from src.blueprints.api.brand_routes import brand
 
 
 def init_app(app):
     app.register_blueprint(user)
     app.register_blueprint(login)
+    app.register_blueprint(brand)
