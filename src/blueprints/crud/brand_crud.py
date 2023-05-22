73cf9531dec404eb45bc2b6aa07df08e59195c04
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-22 16:12:23 -0300

    fix: Refatoração para práticas de Clean Code

--- src/blueprints/crud/brand_crud.py
+++ src/blueprints/crud/brand_crud.py
@@ -6,35 +6,35 @@ from src.blueprints.schemas.brand_schema import BrandSchema
 class BrandCRUD:
 
     def create_brand(self, payload: dict):
+        schema = BrandSchema()
         try:
             name = payload['name'].capitalize()
             code = payload['code'].upper()
             brand = BrandModel(name=name, code=code)
             db.session.add(brand)
             db.session.commit()
-            schema = BrandSchema()
             return f'Brand successfully created: {schema.dump(brand)}'
         except Exception as exc:
             print(f'BrandCRUD[CREATE_BRAND] Error: {exc}')
             return f'Create Error.'
 
     def read_brand(self, id: int):
+        schema = BrandSchema()
         try:
             brand = BrandModel.query.get(id)
             if brand is None:
                 return 'Brand not found', 404
-            schema = BrandSchema()
             return f'Brand: {schema.dump(brand)}'
         except Exception as exc:
             print(f'BrandCRUD[READ_BRAND] Error: {exc}')
             return f'Read Error.'
 
     def read_brands(self):
+        schema = BrandSchema(many=True)
         try:
             brands = BrandModel.query.all()
             if brands is None:
                 return 'Brands not found', 404
-            schema = BrandSchema(many=True)
             return f'Brand: {schema.dump(brands)}'
         except Exception as exc:
             print(f'BrandCRUD[READ_BRANDS] Error: {exc}')
